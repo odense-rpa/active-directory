61e63e0ed6523d8028b0441815e69e45979a75c6
Simon Orefici Frederiksen <simof@odense.dk>
2026-04-09 10:50:57 +0200

    Fixed goof

--- active_directory/client.py
+++ active_directory/client.py
@@ -14,8 +14,7 @@ class ActiveDirectoryClient:
         self.server = Server(server_url, port=port)
         self.connection = Connection(self.server, user=username, password=password, auto_bind=True)
         self.base_dn = base_dn
-        self.search_filter = f"(sAMAccountName={username.split('@')[0]})"
-        
-    def søg(self, attributes: list):
-        self.connection.search(self.base_dn, self.search_filter, attributes=attributes)
-        return self.connection.entries
+
+        def søg(self, søgefilter: str, attributes: list):
+            self.connection.search(self.base_dn, search_filter=søgefilter, attributes=attributes)
+            return self.connection.entries
